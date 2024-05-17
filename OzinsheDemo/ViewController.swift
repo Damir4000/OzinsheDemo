4e2a7471fcce2059f0d8d1658adfb03840d9cc23
Damir Nurgaliev <nurgaliev450@gmail.com>
2024-05-17 20:21:50 +0500

    Initial Commit

---
+++ OzinsheDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OzinsheDemo
+//
+//  Created by Дамир Нургалиев on 17.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
